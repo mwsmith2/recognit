fc01031badbf7e04966a1fc9ed99e7720c6af9d0
Durmus Ugur Karatay <ukaratay@gmail.com>
2013-11-10 18:30:14 -0800

    Plots are added.

--- pca_test.py
+++ pca_test.py
@@ -2,34 +2,64 @@
 
 import analyze
 import load
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
-filename = 'faces/straight_open_4_faces.txt'
+size = ['0', '2', '4']
+t = np.linspace(0.5, 1, 40)
 
-XTrain, XTest, trainlist, testlist = load.createMatrices(filename)
-weightDict, eigvecs, meanface = analyze.characterizeFaces(XTrain, trainlist)
+neig = np.zeros((len(size), len(t)))
+rate = np.zeros((len(size), len(t)))
 
-success = 0
-successlist = []
-successdistance = []
+for i in range(len(size)):
 
-fail = 0
-faillist = []
-faildistance = []
+    filename = 'faces/straight_open_' + size[i] + '_faces.txt'
+    XTrain, XTest, trainlist, testlist = load.createMatrices(filename, sd=659, ratio=0.65)
 
-for imgname in testlist:
+    for j in range(len(t)):
 
-    image = load.readPGM('faces/' + imgname.rstrip('\n'))
-    weight = analyze.calcWeight(image.reshape(-1), eigvecs, meanface)
-    person, distance = analyze.guessLabel(weightDict, weight)
+        weightDict, eigvecs, meanface = analyze.characterizeFaces(XTrain, trainlist, threshold=t[j])
 
-    if person == imgname.split('_')[0]:
+        success = 0
+        fail = 0
 
-        success += 1
-        successlist.append(imgname)
-        successdistance.append(distance)
+        for imgname in testlist:
 
-    else:
+            image = load.readPGM('faces/' + imgname.rstrip('\n'))
+            weight = analyze.calcWeight(image.reshape(-1), eigvecs, meanface)
+            person, distance = analyze.guessLabel(weightDict, weight)
 
-        fail += 1
-        faillist.append(imgname)
-        faildistance.append(distance)
+            if person == imgname.split('_')[0]:
+
+                success += 1
+
+            else:
+
+                fail += 1
+
+        neig[i, j] = len(weight)
+        rate[i, j] = 100 * success / float(success + fail)
+
+plt.plot(t, neig[0, :], 'r', label="High resolution", linewidth=2.0)
+plt.plot(t, neig[1, :], 'g', label="Med resolution", linewidth=2.0)
+plt.plot(t, neig[2, :], 'b', label="Low resolution", linewidth=2.0)
+plt.xlim((0.5, 1))
+plt.xlabel('Threshold')
+plt.ylabel('Number of Eigenvectors')
+plt.legend(loc=0)
+
+plt.savefig("neig.pdf")
+plt.close()
+
+plt.plot(t, rate[0, :], 'r', label="High resolution", linewidth=2.0)
+plt.plot(t, rate[1, :], 'g', label="Med resolution", linewidth=2.0)
+plt.plot(t, rate[2, :], 'b', label="Low resolution", linewidth=2.0)
+plt.xlim((0.5, 1))
+plt.xlabel('Threshold')
+plt.ylabel('Success Rate [%]')
+plt.legend(loc=0)
+
+plt.savefig("rate.pdf")
+plt.close()
