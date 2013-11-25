e9c9814bf355a9111f90d35ca44dcc67b515d0a2
Durmus Ugur Karatay <ukaratay@gmail.com>
2013-11-24 16:38:31 -0800

    Implemented PCA in object-oriented, as well as LDA.

---
+++ src/analysis.py
@@ -0,0 +1,159 @@
+"""pca.py: Contains a class for Principal Component Analysis (PCA)."""
+
+__author__ = "Durmus U. Karatay, Matthias W. Smith"
+__email__ = "ukaratay@uw.edu, mwsmith2@uw.edu"
+
+import numpy as np
+from scipy.linalg import eig, norm
+from scipy.spatial.distance import cdist
+
+
+class PCA:
+    
+    def __init__(self, x):        
+
+        # Initialize x and average face.
+        self.x = x
+        self.d, self.N = x.shape 
+        self.mu = x.mean(axis=-1).reshape(self.d, 1)
+        
+    def findPrincipalComponents(self, threshold=0.90):
+        
+        # Normalize x by subtracting average face.
+        self.x -= self.mu
+        
+        # Get eigenvalues and vectors of cov(x.T * x).
+        cov = np.dot(self.x.T, self.x)
+        eigval, eigvec = eig(cov)
+        
+        # Convert these to eigenvalues of the cov(X).
+        eigvec = np.dot(self.x, eigvec)
+        # Sort them in descending order.
+        eigvec = eigvec[:, eigval.argsort()][:, ::-1]
+        eigval.sort()
+        eigval = eigval[::-1]
+        
+        # Select the first k ones, which are responsible of 95% variance.
+        eignorm = eigval.cumsum() / eigval.sum()
+        self.k = np.abs(eignorm - threshold).argmin() 
+
+        # Normalize the eigenfaces.
+        for i in range(self.k):
+
+            eigvec[:, i] = eigvec[:, i] / norm(eigvec[:, i])
+            
+        self.eigvec = eigvec[:, :self.k]
+        self.eigval = eigval[:self.k]
+        
+    def project(self, s):
+        
+        # Check shape and reshape if necessary.
+        if len(s.shape) == 1:
+            
+            s = s.reshape(len(s), 1)        
+        
+        # Project s onto eigenfaces.
+        weight = np.dot(self.eigvec.T, s - self.mu)
+            
+        return weight
+            
+    def createDatabase(self, y):
+        
+        # Create a database of faces given list of features.
+        self.y = np.array(y)
+        self.weights = np.dot(self.eigvec.T, self.x)
+        
+    def predict(self, s, method='1NN'):
+        
+        # Calculate weights for given image.
+        weight = self.project(s)
+            
+        # Predict by given method.
+        if (method == '1NN'):
+            
+            distance = cdist(self.weights.T, weight.T, 'mahalanobis')
+            prediction = self.y[distance.argmin()].tolist()
+            
+        return prediction
+
+class LDA():
+    
+    def __init__(self, x, c):
+        
+        # Initialize x and average face.
+        self.x = x
+        self.d, self.N = x.shape
+        self.mu = x.mean(axis=-1).reshape(self.d, 1)
+        
+        # Initialize classes and class averages.        
+        self.c = np.array(c)
+        self.ulabels = np.unique(c)
+        self.muc = np.empty((self.d, len(self.ulabels)))
+        
+        for i in range(len(self.ulabels)):
+    
+            self.muc[:, i] = x[:, c == self.ulabels[i]].mean(axis=-1)
+        
+    def findComponents(self, pcavec, k=0):
+        
+        if (k <= 0 or k > (len(self.ulabels) - 1)):
+            
+            k = len(self.ulabels) - 1
+            
+        self.k = k
+        
+        sw = np.zeros((self.d, self.d))
+        sb = np.zeros((self.d, self.d))
+        
+        for i in range(len(self.ulabels)):
+            
+            cls = self.x[:, self.c == self.ulabels[i]]
+            mcls = self.muc[:, i].reshape(self.d, 1)
+            
+            sw += np.dot((cls - mcls), (cls - mcls).T)
+            sb += self.N * np.dot((mcls - self.mu), (mcls - self.mu).T)
+        
+        eigval, eigvec = eig(np.dot(np.linalg.inv(sw), sb))
+        
+        eigvec = eigvec[:, eigval.argsort()][:, ::-1]
+        eigval.sort()
+        eigval = eigval[::-1]
+        
+        self.eigvec = np.dot(pcavec, eigvec[:, :self.k]).real
+        self.eigval = eigval[:self.k].real
+
+    def project(self, s):
+        
+        # Check shape and reshape if necessary.
+        if len(s.shape) == 1:
+            
+            s = s.reshape(len(s), 1)        
+        
+        # Project s onto eigenfaces.
+        weight = np.dot(self.eigvec.T, s - self.mufaces)
+            
+        return weight
+            
+    def createDatabase(self, x):
+        
+        # Create a database of faces.
+        self.faces = x
+        self.mufaces = self.faces.mean(axis=-1).reshape(len(x), 1)
+        self.weights = np.dot(self.eigvec.T, self.faces)            
+            
+    def predict(self, s, method='1NN'):
+    
+        # Calculate weights for given image.
+        weight = self.project(s)
+        
+        # Predict by given method.
+        if (method == '1NN'):
+        
+            distance = cdist(self.weights.T, weight.T, 'mahalanobis')
+            prediction = self.c[distance.argmin()].tolist()
+        
+        return prediction
+        
+        
+    
+        
---
+++ src/pca_test2.py
@@ -0,0 +1,58 @@
+import load
+from pca import PCA, LDA
+
+
+filename = 'faces/straight_open_0_faces.txt'
+XTrain, XTest, trainlist, testlist = load.createMatrices(filename, sd=659, ratio=0.65)
+
+clf = PCA(XTrain)
+clf.findPrincipalComponents()
+clf.createDatabase(trainlist)
+
+success = 0
+fail = 0
+
+for imgname in testlist:
+
+    image = load.readPGM('faces/' + imgname.rstrip('\n'))
+    person = clf.predict(image.reshape(-1))
+
+    if person.split('_')[0] == imgname.split('_')[0]:
+
+        success += 1
+
+    else:
+
+        fail += 1
+
+    neig = clf.k
+    rate = 100 * success / float(success + fail)
+
+c = []
+for string in trainlist:
+    
+    c.append(string.split('_')[0])
+
+clLDA = LDA(clf.weights, c)
+clLDA.findComponents(clf.eigvec)
+clLDA.createDatabase(clf.x)
+    
+successLDA = 0
+failLDA = 0
+
+for imgname in testlist:
+
+    image = load.readPGM('faces/' + imgname.rstrip('\n'))
+    person = clLDA.predict(image.reshape(-1))
+    
+    
+    if person == imgname.split('_')[0]:
+
+        successLDA += 1
+
+    else:
+
+        failLDA += 1
+
+    neig = clf.k
+    rate = 100 * successLDA / float(successLDA + failLDA)
