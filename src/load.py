5ad4bf6f031b72b1f739f4d54e68c84d64c94c2c
Matthias Smith <mwsmith2@uw.edu>
2013-12-01 18:20:36 -0800

    Added a data container class.  It handles whitelisting and blacklisting certain labels to create new data sets without reloading all the images.

--- src/load.py
+++ src/load.py
@@ -195,3 +195,159 @@ def createMatrices(filename, sd=1234, ratio=0.6):
             XTest[:, j] = image.reshape(-1)
 
     return XTrain, XTest, trainlist, testlist
+
+class Faces():
+
+    def __init__(self, filename, sd=1234, ratio=0.6):
+
+        self.XTrain, self.XTest, trainlist, testlist = createMatrices(filename, sd=sd, ratio=ratio) 
+
+        self.YTrain = []
+        for labels in trainlist:
+            self.YTrain.append(labels[6:-5].split('_'))
+            # 6:-5 cuts off faces/ prefix and .pgd suffix  
+
+        self.YTest = []
+        for labels in testlist:
+            self.YTest.append(labels[6:-5].split('_'))
+
+        self.XTrainCut = self.XTrain
+        self.XTestCut  = self.XTest
+        self.YTrainCut = list(self.YTrain)
+        self.YTestCut  = list(self.YTest)
+
+        # A few more list to hold our cuts
+        self.blacklist = [] # Use to cut from data
+        self.whitelist = []
+        for s in self.YTrain[0]:
+            self.whitelist.append("")
+
+        # Store the current number of samples
+        self.nTestPoints = self.XTestCut.shape[1]
+        self.nTrainPoints = self.XTrainCut.shape[1]
+        self.upToDate = True
+
+    def flag(self, badLabel):
+
+        if isinstance(badLabel, list):
+            for label in badLabel:
+                self.blacklist.append(label)
+        else:
+            self.blacklist.append(badLabel)
+
+        self.upToDate = False
+
+    def clearFlags(self):
+
+        self.blacklist = []
+
+        self.upToDate  = False
+
+    def force(self, labelID, label=""):
+
+        try: 
+            self.whitelist[labelID] = label
+
+        except IndexError:
+            print "No such label ID."
+            pass
+
+        self.upToDate = False
+
+    def clearForce(self):
+
+        self.whitelist = []
+        for s in self.YTrain[0]:
+            self.whitelist.append('')
+
+        self.upToDate = False
+
+    def build(self):
+
+        # Check if needs to be rebuilt
+        if self.upToDate:
+            return
+
+        # Rebuild the data and labels with cuts
+        cutIdx = 0
+        oldIdx = 0
+        for y in self.YTrain:
+
+            flagged = False
+
+            for label in y:
+                if label in self.blacklist:
+                    flagged = True
+
+            for w in self.whitelist:
+                if (w != '' and w not in y):
+                    flagged = True
+
+            if flagged:
+                oldIdx += 1
+                continue
+
+            print cutIdx, oldIdx
+
+            self.XTrainCut[:, cutIdx] = self.XTrain[:, oldIdx]
+            self.YTrainCut[cutIdx] = y
+
+            cutIdx += 1
+            oldIdx += 1
+
+        self.nTrainPoints = cutIdx
+
+        cutIdx = 0
+        oldIdx = 0
+        for y in self.YTest:
+
+            flagged = False
+
+            for label in y:
+                if label in self.blacklist:
+                    flagged = True
+
+
+            for w in self.whitelist:
+                if (w != '' and w not in y):
+                    flagged = True
+
+            if flagged:
+                oldIdx += 1
+                continue
+
+            print cutIdx, oldIdx
+
+            self.XTestCut[:, cutIdx] = self.XTest[:, oldIdx]
+            self.YTestCut[cutIdx] = y
+
+            cutIdx += 1
+            oldIdx += 1
+
+        self.nTestPoints = cutIdx
+        self.upToDate = True
+
+    def getTrain(self, labelID):
+
+        self.build()
+        YTrain = []
+        for y in self.YTrainCut:
+            YTrain.append(y[labelID])
+
+        return self.XTrainCut[:, :self.nTrainPoints], YTrain[:self.nTrainPoints]
+
+    def getTest(self, labelID):
+
+        self.build()
+        YTest = []
+        for y in self.YTestCut:
+            YTest.append(y[labelID])
+
+        return self.XTestCut[:, :self.nTestPoints], YTest[:self.nTestPoints]
+
+
+
+
+
+
+
