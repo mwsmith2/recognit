3226e8c8133e268eac716ee8e2f4b024981f6075
Matthias W. Smith <mwsmith2@uw.edu>
2014-12-02 20:17:18 -0800

    Cleaning up all the baggage from the class. I removed all the posters and reports.  I removed the data from the repository, since it doesn't need to come bundled. I kept the references for now.

---
+++ src/faceReconstruction.py
@@ -0,0 +1,46 @@
+import matplotlib.cm as cm
+from sklearn.lda import LDA
+import numpy as np
+from collections import defaultdict
+from scipy.spatial.distance import cdist
+import matplotlib.pyplot as plt
+
+import load
+import pca
+import predict
+import visual as vis
+
+path = '../data/faces'
+
+# Load faces from the path.
+faces = load.Faces(path, 'pgm')
+faces.setParams(seed=895, train=0.60, valid=0.15)
+faces.getLabels()
+
+res = 0
+trait = 0
+nweights = 5 # plots all weights less against each other
+
+prefix = 'res_' + str(res) + '_trait_' + str(trait) + '_'
+reslist = ['0', '2', '4']
+reslist.remove(str(res)) 
+
+faces.getData(ID=trait, exclude={4: reslist})
+faces.createMatrices()
+
+# Start principal component analysis.
+clf = pca.PCA(faces.XTrain)
+clf.findPrincipalComponents()
+clf.transform()
+
+plt.imshow(faces.XTest[:, 0].reshape([120, 128]))
+plt.savefig('original_face.pdf')
+WRecon = clf.project(faces.XTest[:, 0])
+
+for i in range(clf.k+1):
+	plt.close()
+	X = np.dot(clf.eigvec[:, :i], WRecon[:i]).sum(axis=1).reshape([120, 128])
+	plt.imshow(X + clf.mu.reshape([120, 128]))
+	if i % 10 == 0:
+		plt.savefig('recon_' + str(i) + '.pdf')
+ 
---
+++ src/quickplot.py
@@ -0,0 +1,56 @@
+import matplotlib
+matplotlib.use('PDF')
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+from sklearn.lda import LDA
+import numpy as np
+from collections import defaultdict
+from scipy.spatial.distance import cdist
+
+import load
+import pca
+import predict
+import visual as vis
+
+path = '../data/faces'
+
+# Load faces from the path.
+faces = load.Faces(path, 'pgm')
+faces.setParams(seed=895, train=0.60, valid=0.15)
+faces.getLabels()
+
+faces.getData(ID=0, exclude={4: ['2', '4']})
+faces.createMatrices()
+
+# Start principal component analysis.
+clf = pca.PCA(faces.XTrain)
+clf.findPrincipalComponents()
+clf.transform()
+
+res = 0
+trait = 0
+
+d = []
+x1 = []
+x2 = []
+
+for i in range(len(faces.YValid)):
+	s  = clf.project(faces.XValid[:, i])
+	d.append(np.min(cdist(clf.xtransform.T, s.T, "mahalanobis")))
+	x1.append(s[0])
+	x2.append(s[1])
+
+for trait in range(4):
+	prefix = 'res_' + str(res) + '_trait_' + str(trait) + '_'
+	faces.setParams(seed=895, train=0.60, valid=0.15)
+	faces.getData(ID=trait, exclude={4: ['2', '4']})
+	faces.createMatrices()
+	faceweights = defaultdict(list)
+	
+	for i, y in enumerate(faces.YValid):
+		faceweights[y].append([x1[i], x2[i], d[i]])
+
+	fn = prefix + "face_scatter_w" + str(1) + "_v_w" + str(2) + ".pdf"
+	title = r'Principal Components: $\omega_1$ vs. $\omega_2$'
+	vis.scatterFace(title, faceweights, filename=fn)
+
